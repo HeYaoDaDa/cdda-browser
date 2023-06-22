b28aabc6680ce43b6c8daa0fe3c0850a57bb604d
HeYaoDaDa <3050166231@qq.com>
2023-06-22 21:06:22 +0800

    reformat JsonFlag

--- src/main/kotlin/fun/hydd/cdda_browser/model/cddaItem/JsonFlag.kt
+++ src/main/kotlin/fun/hydd/cdda_browser/model/cddaItem/JsonFlag.kt
@@ -1,17 +1,16 @@
 package `fun`.hydd.cdda_browser.model.cddaItem
 
 import `fun`.hydd.cdda_browser.annotation.MapInfo
-import `fun`.hydd.cdda_browser.model.CddaCommonItem
 import `fun`.hydd.cdda_browser.model.base.parent.CddaObject
 import `fun`.hydd.cdda_browser.model.cddaItem.cddaSubObject.CddaItemRef
 import `fun`.hydd.cdda_browser.model.cddaItem.cddaSubObject.Translation
 
+@Suppress("unused")
 class JsonFlag : CddaObject() {
-  lateinit var id: String
   var info: Translation? = null
   var inherit: Boolean = true
   var craftInherit: Boolean = false
-  var tasteMod: Double? = null
+  var tasteMod: Int = 0
   var restriction: Translation? = null
   var name: Translation? = null
 
@@ -20,8 +19,4 @@ class JsonFlag : CddaObject() {
 
   @MapInfo(param = "JSON_FLAG")
   var requiresFlag: CddaItemRef? = null
-
-  override fun finalize(commonItem: CddaCommonItem, itemRef: CddaItemRef) {
-    this.itemName = this.name
-  }
 }
