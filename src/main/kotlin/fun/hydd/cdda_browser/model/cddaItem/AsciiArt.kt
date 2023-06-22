36c7943092f72884a32138b0af38f534059257c9
HeYaoDaDa <3050166231@qq.com>
2023-06-22 21:15:28 +0800

    reformat AsciiArt

--- src/main/kotlin/fun/hydd/cdda_browser/model/cddaItem/AsciiArt.kt
+++ src/main/kotlin/fun/hydd/cdda_browser/model/cddaItem/AsciiArt.kt
@@ -1,27 +1,10 @@
 package `fun`.hydd.cdda_browser.model.cddaItem
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import `fun`.hydd.cdda_browser.annotation.IgnoreMap
 import `fun`.hydd.cdda_browser.annotation.MapInfo
-import `fun`.hydd.cdda_browser.model.CddaCommonItem
 import `fun`.hydd.cdda_browser.model.base.parent.CddaObject
-import `fun`.hydd.cdda_browser.model.cddaItem.cddaSubObject.CddaItemRef
 
+@Suppress("unused")
 class AsciiArt : CddaObject() {
-  lateinit var id: String
-
-  @IgnoreMap
-  lateinit var picture: String
-
-  @JsonIgnore
   @MapInfo(key = "picture")
-  var pictureJson: MutableList<String> = mutableListOf()
-  override fun finalize(commonItem: CddaCommonItem, itemRef: CddaItemRef) {
-    val stringBuilder = StringBuilder()
-    this.pictureJson.forEachIndexed { index, s ->
-      stringBuilder.append(s)
-      if (index < this.pictureJson.size - 1) stringBuilder.append("\n")
-    }
-    this.picture = stringBuilder.toString()
-  }
+  lateinit var picture: MutableList<String>
 }
