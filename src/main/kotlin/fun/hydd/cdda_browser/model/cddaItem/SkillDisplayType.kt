17e126bba65cbddbb9134b674b2d13d8a777cc3a
HeYaoDaDa <3050166231@qq.com>
2023-06-22 22:48:21 +0800

    add SkillDisplayType

---
+++ src/main/kotlin/fun/hydd/cdda_browser/model/cddaItem/SkillDisplayType.kt
@@ -0,0 +1,8 @@
+package `fun`.hydd.cdda_browser.model.cddaItem
+
+import `fun`.hydd.cdda_browser.model.base.parent.CddaObject
+import `fun`.hydd.cdda_browser.model.cddaItem.cddaSubObject.Translation
+
+class SkillDisplayType : CddaObject() {
+  lateinit var displayString: Translation
+}
